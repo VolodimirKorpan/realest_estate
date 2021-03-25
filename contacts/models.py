7aad1c327c38875c16e791bab9ce385591c360c4
VolodymyrKorpan <z3tsu1@gmail.com>
2021-03-25 17:55:50 +0200

    First commit with front and back

---
+++ contacts/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from datetime import datetime
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    message = models.TextField(blank=True)
+    contact_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    class Meta:
+        managed = False
+
+    def __str__(self):
+        return self.email
